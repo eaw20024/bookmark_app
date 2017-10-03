6adc11bc9f8ac78cc5fa0c740b67097bef2d29fe
Eric Washington <Washington_Eric@bah.com>
2017-10-03 16:14:57 -0400

    Server.js

--- server.js
+++ server.js
@@ -10,26 +10,44 @@
       }
     }
     input.addEventListener('focusin', showFloater);
-    input.addEventListener('focusout', closeFloater);
+    // input.addEventListener('focusout', closeFloater);
     overlay.addEventListener('click', closeFloater);
     // =========================
     const bookmarksList = document.querySelector('.bookmarks-list');
     const bookmarkForm  = document.querySelector('.bookmark-form');
     const bookmarkInput = bookmarkForm.querySelector('input[type=text]');
     const bookmarks     = JSON.parse(localStorage.getItem('bookmarks')) || [];
+    const apiUrl        = 'https://opengraph.io/api/1.0/site';
+    const appId         = '58858c7bcf07b61e64257391'; 
     fillBookmarksList(bookmarks);
     function createBookmark(e) {
       e.preventDefault();
+      if (!bookmarkInput.value) {
+        alert('We need info!');   
+        return;
+      }
+      const url = encodeURIComponent(bookmarkInput.value);
       // add a new bookmark to the bookmarks
-      const title    = bookmarkInput.value;    
-      const bookmark = {
-        title: title
-      };
-      bookmarks.push(bookmark);
-      fillBookmarksList(bookmarks);
-      storeBookmarks(bookmarks);
-      bookmarkForm.reset();
-      console.table(bookmarks);
+      fetch(`${apiUrl}/${url}?app_id=${appId}`)
+      // fetch(apiUrl + '/' + url + '?app_id=' + appId)
+        .then(response => response.json())
+        .then(data => { 
+          console.log(data);
+          const bookmark = {
+            title: data.hybridGraph.title,
+            image: data.hybridGraph.image,
+            link: data.hybridGraph.url
+          };
+          console.log(bookmark);
+          bookmarks.push(bookmark);
+          fillBookmarksList(bookmarks);
+          storeBookmarks(bookmarks);
+          bookmarkForm.reset();
+        })
+        .catch(error => {
+          alert('There was a problem getting info!'); 
+        });
+      // console.table(bookmarks);
       // save that bookmarks list to localStorage
       // const title        = bookmarkInput.value;      
       // const bookmark     = document.createElement('a');
@@ -40,10 +58,12 @@
       // bookmarksList.appendChild(bookmark);      
     }
     function fillBookmarksList(bookmarks = []) {
-      const bookmarksHtml = bookmarks.map((bookmark) => {
+      const bookmarksHtml = bookmarks.map((bookmark, i) => {
         return `
-          <a href="#" class="bookmark">
-            ${bookmark.title}
+          <a href="${bookmark.link}" class="bookmark" data-id="${i}">
+            <div class="img" style="background-image:url('${bookmark.image}')"></div>
+            <div class="title">${bookmark.title}</div>
+            <span class="glyphicon glyphicon-remove"></span>
           </a>
         `;
       }).join('');     
@@ -56,10 +76,21 @@
       //     </a>
       //   `;
       // }
-      // console.log(bookmarksHtml);  
-      
+      // console.log(bookmarksHtml);        
+    }
+    function removeBookmark(e) {
+      if (!e.target.matches('.glyphicon-remove')) return;
+      // find the index
+      // remove from the bookmarks using splice
+      // fill the list
+      // store back to localStorage
+      const index = e.target.parentNode.dataset.id;
+      bookmarks.splice(index, 1);
+      fillBookmarksList(bookmarks);
+      storeBookmarks(bookmarks); 
     }
     function storeBookmarks(bookmarks = []) {
       localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
     }
     bookmarkForm.addEventListener('submit', createBookmark);
+    bookmarksList.addEventListener('click', removeBookmark);
